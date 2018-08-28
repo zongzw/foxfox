3bebf34413d3e8f28beb0bcb29ac96458c67bcac
Andrew ZONG <a.zong@f5.com>
2018-08-28 09:41:58 +0800

    initial commit

---
+++ server.py
@@ -0,0 +1,29 @@
+import os
+try:
+  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+  from SocketServer import TCPServer as Server
+except ImportError:
+  from http.server import SimpleHTTPRequestHandler as Handler
+  from http.server import HTTPServer as Server
+
+import os
+import ssl
+
+# Read port selected by the cloud for our application
+PORT = int(os.getenv('PORT', 8000))
+
+# Change current directory to avoid exposure of control files
+
+httpd = Server(("", PORT), Handler)
+httpd.socket = ssl.wrap_socket(httpd.socket, 
+            keyfile="private.key", 
+            certfile="certificate.crt", server_side=True)
+
+os.chdir('static')
+try:
+  print("Start serving at port %i" % PORT)
+  httpd.serve_forever()
+except KeyboardInterrupt:
+  pass
+httpd.server_close()
+
