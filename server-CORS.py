be330f09dd1ece32c7507a4c6789bca2aeac1a53
Andrew ZONG <a.zong@f5.com>
2018-09-29 10:45:40 +0800

    Add server-xxx for different senario.

---
+++ server-CORS.py
@@ -0,0 +1,68 @@
+import os
+try:
+  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+  from SocketServer import TCPServer as Server
+except ImportError:
+  from http.server import SimpleHTTPRequestHandler as Handler
+  from http.server import HTTPServer as Server
+
+import os
+
+# Read port selected by the cloud for our application
+PORT = int(os.getenv('PORT', 8000))
+
+class CORSHTTPRequestHandler(Handler):
+    def send_head(self):
+        """Common code for GET and HEAD commands.
+        This sends the response code and MIME headers.
+        Return value is either a file object (which has to be copied
+        to the outputfile by the caller unless the command was HEAD,
+        and must be closed by the caller under all circumstances), or
+        None, in which case the caller has nothing further to do.
+        """
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            if not self.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(301)
+                self.send_header("Location", self.path + "/")
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
+        try:
+            # Always read in binary mode. Opening files in text mode may cause
+            # newline translations, making the actual size of the content
+            # transmitted *less* than the content-length!
+            f = open(path, 'rb')
+        except IOError:
+            self.send_error(404, "File not found")
+            return None
+        self.send_response(200)
+        self.send_header("Content-type", ctype)
+        fs = os.fstat(f.fileno())
+        self.send_header("Content-Length", str(fs[6]))
+        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+        
+        return f
+
+httpd = Server(("", PORT), CORSHTTPRequestHandler)
+
+# Change current directory to avoid exposure of control files
+os.chdir('static')
+try:
+  print("Start serving at port %i" % PORT)
+  httpd.serve_forever()
+except KeyboardInterrupt:
+  pass
+httpd.server_close()
+
--- server-default.py
+++ server-default.py
@@ -7,18 +7,13 @@ except ImportError:
   from http.server import HTTPServer as Server
 
 import os
-import ssl
 
 # Read port selected by the cloud for our application
 PORT = int(os.getenv('PORT', 8000))
 
-# Change current directory to avoid exposure of control files
-
 httpd = Server(("", PORT), Handler)
-httpd.socket = ssl.wrap_socket(httpd.socket, 
-            keyfile="private.key", 
-            certfile="certificate.crt", server_side=True)
 
+# Change current directory to avoid exposure of control files
 os.chdir('static')
 try:
   print("Start serving at port %i" % PORT)
